c38076c01dd870a13bb6f77c2514bc0b91b9407c
mdd300 <victoroshiro@air-de-victor.home>
2017-05-10 09:58:40 -0300

    Tela de login

---
+++ QRGO/app/src/main/java/com/uniquesys/qrgo/MainActivity.java
@@ -0,0 +1,40 @@
+package com.uniquesys.qrgo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    EditText user,senha;
+    String login_name,login_pass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void login(View v) {
+        user = (EditText)findViewById(R.id.txtUser);
+        senha = (EditText) findViewById(R.id.txtSenha);
+        login_name = user.getText().toString();
+        login_pass = senha.getText().toString();
+        String method = "https://www.uniquesys.com.br/qrgo/login/efetuar_login";
+        Model loginTask = new Model(this);
+        loginTask.execute(method, login_name, login_pass);
+    }
+
+    @Override
+    public void onClick(View v) {
+        user = (EditText)findViewById(R.id.txtUser);
+        senha = (EditText) findViewById(R.id.txtSenha);
+        login_name = user.getText().toString();
+        login_pass = senha.getText().toString();
+        String method = "https://www.uniquesys.com.br/qrgo/login/efetuar_login";
+        Model loginTask = new Model(this);
+        loginTask.execute(method, login_name, login_pass);
+
+    }
+}
---
+++ QRGO/app/src/main/java/com/uniquesys/qrgo/Model.java
@@ -0,0 +1,107 @@
+package com.uniquesys.qrgo;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+/**
+ * Created by victoroshiro on 09/05/17.
+ */
+
+public class Model extends AsyncTask<String,Void,String> {
+    AlertDialog alertDialog;
+    Context ctx;
+    Model(Context ctx)
+    {
+        this.ctx =ctx;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        alertDialog = new AlertDialog.Builder(ctx).create();
+        alertDialog.setTitle("Login Information....");
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String method = params[0];
+        String login_url = method;
+        // String reg_url = "http://192.168.56.1/webapp/register.php";
+        // String login_url = "http://192.168.56.1/webapp/login.php";
+
+
+            String login_name = params[1];
+            String login_pass = params[2];
+            try {
+                URL url = new URL(login_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
+                String data = URLEncoder.encode("email-login","UTF-8")+"="+URLEncoder.encode(login_name,"UTF-8")+"&"+
+                        URLEncoder.encode("senha-login","UTF-8")+"="+URLEncoder.encode(login_pass,"UTF-8")+"&"+
+                        URLEncoder.encode("format","UTF-8")+"="+URLEncoder.encode(String.valueOf(true),"UTF-8");
+                bufferedWriter.write(data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                String response = "";
+                String line  = "";
+                while ((line = bufferedReader.readLine())!=null)
+                {
+                    response+= line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return response;
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        if(result.equals("Registration Success..."))
+        {
+            Toast.makeText(ctx, result, Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            alertDialog.setMessage(result);
+            alertDialog.show();
+        }
+
+    }
+}
